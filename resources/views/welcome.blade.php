2d2a044a68012b44e5fef5a2386ab15fb20a6c5b
Samsudheen Saludheen <samsudheen.saludheen@remotea.com>
2018-11-20 14:36:02 +0200

    cap deploy test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5
+                    Laravel
                 </div>
 
                 <div class="links">
